d3f1d669638c62ba68a489f1de18869e41f8f1d8
hansmong <lrcry@vip.qq.com>
2015-10-18 13:18:29 +1100

    complete view job application

--- WebContent/ng-app/orsController.js
+++ WebContent/ng-app/orsController.js
@@ -4,21 +4,34 @@ var orsCtrler = angular.module('orsController', ['ngSanitize', 'ngResource']);
  * Home page controller
  */
 orsCtrler.controller('HomeController', ['$http', '$scope', function($http, $scope){
-	$http.get('//localhost:8080/HRMgmtSysREST/jobPostings').success(function(data) {
+	$http.get(
+		'//localhost:8080/HRMgmtSysREST/jobPostings'
+	).success(function(data) {
 		$scope.jobPostings = data;
 		$scope.isHomePage = true;
 		console.log($scope.isHomePage);
 	});
 }])
 
-orsCtrler.controller('JobDetailController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams){
-	$http.get('//localhost:8080/HRMgmtSysREST/jobPostings/' + $routeParams._jobId).success(function(data) {
+/**
+ * Job details controller
+ */
+orsCtrler.controller('JobDetailController', ['$http', '$scope', '$routeParams', 
+	function($http, $scope, $routeParams){
+	$http.get(
+		'//localhost:8080/HRMgmtSysREST/jobPostings/' + $routeParams._jobId
+	).success(function(data) {
 		$scope.jobPosting = data.jobPosting;
-		console.log('_jobId=' + $routeParams._jobId + ', returnJobId=' + $scope.jobPosting._jobId + ', status=' + $scope.jobPosting.status);
+		console.log('_jobId=' + $routeParams._jobId + ', returnJobId=' 
+			+ $scope.jobPosting._jobId + ', status=' + $scope.jobPosting.status);
 	});
 }])
 
-orsCtrler.controller('JobApplyController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
+/** 
+ * Apply for a job controller
+ */
+orsCtrler.controller('JobApplyController', ['$http', '$scope', '$routeParams', 
+	function($http, $scope, $routeParams) {
 	$scope.jobId = $routeParams._jobId;
 	$scope.jobApplyObject = {};
 	$scope.processApply = function() {
@@ -48,6 +61,55 @@ orsCtrler.controller('JobApplyController', ['$http', '$scope', '$routeParams', f
 	};
 }])
 
-orsCtrler.controller('ViewApplicationController', ['$http', '$scope', function($http, $scope) {
-	alert("application!");
+/**
+ * View application controller
+ */
+orsCtrler.controller('ViewApplicationController', ['$http', '$scope', 
+	function($http, $scope) {
+	$scope.getApplication = function() {
+		console.log("appid=" + $scope.viewApp._appId);
+		var oldInputId = $scope.viewApp._appId;
+		$http.get(
+			'//localhost:8080/HRMgmtSysREST/jobapplications/' + $scope.viewApp._appId
+		).success(function(data) {
+			$scope.success = true;
+			$scope.app = data;
+			$scope.oldId = oldInputId;
+			$scope.currentStatus = data.application.status;
+			$http.get(
+				'//localhost:8080/HRMgmtSysREST/jobPostings/' + data.application._jobId
+			).success(function(jobData) {
+				$scope.job = jobData;
+			});
+		}).error(function(err) {
+			$scope.success = false;
+			$scope.message = "Failed to view application. Reason: " + err.errMessage;
+			$scope.errCode = err.errCode;
+			$scope.oldId = oldInputId;
+		});
+	}
+}])
+
+/**
+ * Update application controller
+ */
+orsCtrler.controller('UpdateApplicationController', ['$http', '$scope', 
+	function($http, $scope, $routeParams) {
+
+}])
+
+/**
+ * Cancel application controller
+ */
+orsCtrler.controller('CancelApplicationController', ['$http', '$scope', 
+	function($http, $scope) {
+	
+}])
+
+/**
+ * Archive application controller
+ */
+orsCtrler.controller('ArchiveApplicationController', ['$http', '$scope', 
+	function($http, $scope) {
+	
 }])
